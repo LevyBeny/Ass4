8a3b0a04d44eda678cc930c51bfada3ab4cf4e3c
levybeny <ySsdsp9j>
2017-06-19 12:54:12 +0300

    changesss

--- Ass4/PreProcess.py
+++ Ass4/PreProcess.py
@@ -15,7 +15,7 @@ class PreProcess(object):
             if (self.__structure[column]=='NUMERIC'):
                 train[column].fillna(train[column].mean(), inplace=True)
             else:
-                train[column].fillna((train[column]).mode().iloc[0], inplace=True)
+                train[column].fillna((train[column]).mode()[0], inplace=True)
         return train
 
     # discretize the given train set to given number of bins
@@ -24,7 +24,7 @@ class PreProcess(object):
         for column in self.__structure.keys():
             if (self.__structure[column]=='NUMERIC'):
                 self.__breakPoints[column]=self.__createBreakPoints(train[column],numOfBins)
-                self.__structure[colName]=range(numOfBins)# update structure
+                self.__structure[column]=range(numOfBins)# update structure
                 train[column]=self.__binning(train[column],self.__breakPoints[column],numOfBins)
         return train
 
@@ -32,6 +32,7 @@ class PreProcess(object):
     def discretizeTest(self,test):
         for column in self.__breakPoints.keys():
             test[column]=self.__binning(test[column],self.__breakPoints[column],self.__numOfBins)
+        print(test)
         return test
 
     def __createBreakPoints(self,col,numOfBins):
@@ -41,11 +42,12 @@ class PreProcess(object):
         interval_size=float(maxval-minval)/float(numOfBins)
 
         # Create list of break points
-        break_points=[]
-        i=minval
-        for j in range(numOfBins+1):
-            break_points.append(i)
+        cut_points=[]
+        i=minval+interval_size
+        for j in range(numOfBins-1):
+            cut_points.append(i)
             i+=interval_size
+        break_points = [-float("inf")] + cut_points + [float("inf")]
         return break_points
 
     def __binning(self,col,break_points,numOfBins):
@@ -54,7 +56,7 @@ class PreProcess(object):
         labels = range(numOfBins)
 
         # Binning using cut function of pandas
-        colBin = pd.cut(col, bin=break_points, labels=labels, include_lowest=True)
+        colBin = pd.cut(col, bins=break_points, labels=labels, include_lowest=True)
 
         return colBin
 
